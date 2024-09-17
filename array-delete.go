dd1113cc9ac2db940de9f6b14adc30fbed371de3
najmiddinyodgor-infocom <najmiddinyodgor.infocom@gmail.com>
2024-09-17 16:27:02 +0500

    remove element solution optimized

--- array-delete.go
+++ array-delete.go
@@ -1,22 +1,14 @@
 package main
 
 func RemoveElement(nums []int, val int) int {
-	i, t, r := 0, 0, len(nums)-1
+	cnt := 0
 
-	for i <= r {
-		if nums[i] == val {
-			if nums[r] != val {
-				t = nums[i]
-				nums[i] = nums[r]
-				nums[r] = t
-				i++
-			}
-
-			r--
-		} else {
-			i++
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[i], nums[cnt] = nums[cnt], nums[i]
+			cnt++
 		}
 	}
 
-	return i
+	return cnt
 }
--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	a := []int{1, 2, 3, 3, 5, 2, 6}
-	fmt.Println(SortArrayByParity(a))
+	fmt.Println(RemoveElement(a, 3))
+	fmt.Println(a)
 }
